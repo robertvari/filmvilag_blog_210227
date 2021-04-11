102cac1a0cd98b429d1cb6d6b178db3c7381e8e0
Vári Róbert <mail.robertvari@gmail.com>
2021-04-11 15:10:18 +0200

    get blog list from json server

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import "./BlogCard.css"
 
-const card_data = {
-    id: 1,
-    title: "Szomjas György: „Szabad embereket fényképezünk”",
-    image: "https://m.blog.hu/fi/filmvilag/image/magyar/szomjas_gyorgy.jpg",
-    summary: "80 éves korában elhunyt Szomjas György, Kossuth-díjas és Balázs Béla-díjas rendező, aki olyan meghatározó, stílusteremtő filmeket készített, mint a Talpuk alatt fütyül a szél, a Falfúró, a Roncsfilm, a Kopaszkutya vagy a Könnyű testi sértés. A Filmvilág folyóiratban megjelent interjúiból válogattunk. – Kezdettől fogva a populáris kultúrából építkeztem. Előbb dokumentumfilmeket forgattam a Balázs Béla Stúdióban, és lelkes résztvevője, illetve egyik kezdeményezője voltam annak a bizonyos szociológiai filmprogramnak, amely – sokféle ok miatt – idő előtt megtorpant. Személyes kulturális élményként a rock and rollból és a magyar folklórból táplálkoztam, s a korai betyárfilmjeimben valami hasonló modellt próbáltam követni, amilyet például Sebő Feriék műveltek a zenében. Ugyanakkor arra törekedtem, hogy ne valamiféle idealizált betyárvilágot álmodjak a vászonra, hanem a tényleges alapokhoz nyúltam vissza. Néprajzi anyagokat tanulmányoztam; a Rosszemberek – különben könyvben is megjelent – valóságos alaptörténetét például a kaposvári levéltárból bányásztam elő. Hasonló munka volt az, mint a népzenegyűjtőké.",
-    slug: "szabad-embereket-fenykepezunk"
-}
-
 
 function BlogCard(props) {
+    const {id, title, summary, image} = props.data
+
     return (
         <div className="blog-card-container">
             <div className="card-header">
-                <h1>{card_data.title}</h1>
+                <h1>{title}</h1>
             </div>
 
-
-            <img src={card_data.image} alt=""/>
-            <p>{card_data.summary}</p>
+            <img src={image} alt=""/>
+            <p>{summary}</p>
 
             <div className="card-footer">
                 <a href="">Tovább...</a>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
 import BlogCard from "./BlogCard";
 import "./BlogList.css"
 
 function BlogList(props) {
+    const API_URL = "http://localhost:3001"
+    const [post_list, set_post_list] = useState([])
+
+    const fetch = () => {
+        axios({
+            method: "get",
+            url: `${API_URL}/posts/`
+        })
+            .then(res => set_post_list(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
 
     return (
         <div className="blog-list-container">
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
-            <BlogCard/>
+            {
+                post_list.map(post_data => <BlogCard data={post_data}/>)
+            }
         </div>
     );
 }
