d6052bb9ff8730ceaddbc51ba0cfaf0b3c631a12
Vári Róbert <mail.robertvari@gmail.com>
2021-04-11 16:05:53 +0200

    tweet list/card

--- src/components/SidePanel/TweetList.js
+++ src/components/SidePanel/TweetList.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+function TweetCard(props){
+    const {user, date, content} = props.data
+
+    return(
+        <div className="tweet-card-container">
+            <div className="header">
+                <img src={user.image} alt=""/>
+                <small>{user.name}</small>
+            </div>
+
+            <div className="body">{content}</div>
+
+            <div className="footer">{date}</div>
+        </div>
+    )
+}
 
 function TweetList(props) {
+    const API_URL = process.env.REACT_APP_API_URL
+    const [tweet_list, set_tweet_list] = useState([])
+
+    const fetch = () => {
+        axios({
+            method: "get",
+            url: `${API_URL}/tweets/`
+        })
+            .then(res => set_tweet_list(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
+
     return (
-        <div>Tweets...</div>
+        <div>
+            <h3>Tweets</h3>
+            {
+                tweet_list.map(tweet_data => <TweetCard key={tweet_data.id} data={tweet_data}/>)
+            }
+        </div>
     );
 }
 
